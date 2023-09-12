d10b1c2ce6db6446a1863af8bdd65547de93ffe0
Alejandro (Alex) Manotas <137221150+alexxmanos@users.noreply.github.com>
2023-09-11 22:03:08 -0500

    Update app.js
    Fixed and replaced parameter with URL parameter "app.get('/:number_of_bottles', (req, res) => {
      const numberOfBottles = parseInt(req.params.number_of_bottles, 10); "

--- app.js
+++ app.js
@@ -2,29 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let bottlesOfBeer = 99;
-
 app.get('/', (req, res) => {
-  res.send(`<h1>${bottlesOfBeer} Bottles of beer on the wall</h1>
-            <a href="/take-one-down">Take one down, pass it around</a>`);
+  res.send(`<h1>99 Bottles of beer on the wall</h1>
+            <a href="/98">Take one down, pass it around</a>`);
 });
 
-app.get('/take-one-down', (req, res) => {
-  bottlesOfBeer--;
+app.get('/:number_of_bottles', (req, res) => {
+  const numberOfBottles = parseInt(req.params.number_of_bottles, 10);
 
-  if (bottlesOfBeer === 0) {
+  if (numberOfBottles === 0) {
     res.send(`<h1>No more bottles of beer on the wall</h1>
               <a href="/">Start over</a>`);
   } else {
-    res.send(`<h1>${bottlesOfBeer} Bottles of beer on the wall</h1>
-              <a href="/take-one-down">Take one down, pass it around</a>
+    res.send(`<h1>${numberOfBottles} Bottles of beer on the wall</h1>
+              <a href="/${numberOfBottles - 1}">Take one down, pass it around</a>
               <a href="/">Start over</a>`);
   }
-});
-
-app.get('/reset', (req, res) => {
-  bottlesOfBeer = 99;
-  res.redirect('/');
 });
 
 app.listen(port, () => {
